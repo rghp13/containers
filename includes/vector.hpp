2be29b73b255ca369b48d8b824ffd82ed553247c
Romain PONSONNET <rponsonn@e1r6p10.42.fr>
2022-05-11 18:44:22 +0200

    first push

---
+++ includes/vector.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rponsonn <rponsonn@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/11 16:48:59 by rponsonn          #+#    #+#             */
+/*   Updated: 2022/05/11 17:22:38 by rponsonn         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef VECTOR_HPP
+# define VECTOR_HPP
+#include <memory>//allocator
+
+namespace ft 
+{
+	template <typename T, typename A = std::allocator<T> >
+}
+#endif
